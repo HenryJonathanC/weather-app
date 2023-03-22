e8c2a2e98377b6cc8234a444d7c7c5ddda2f3cec
HenryJonathanC <chenryjonathan2000@gmail.com>
2023-03-22 12:23:13 +0530

    commit

--- src/components/TimeAndLocation.js
+++ src/components/TimeAndLocation.js
@@ -5,7 +5,7 @@ const TimeAndLocation = ({weather}) => {
   return (
     <>
         <div className='flex items-center justify-center mt-1'>
-            <p className='text-white text-xl font-extralight'>{formatToLocalTime(weather.dt, weather.timezone)}</p>
+            <p className='text-white text-xl font-extralight text-center'>{formatToLocalTime(weather.dt, weather.timezone)}</p>
         </div>
         <div className='flex items-center justify-center mt-3'>
             <p className='text-white text-3xl font-medium'>{`${weather.name}, ${weather.country}`}</p>
